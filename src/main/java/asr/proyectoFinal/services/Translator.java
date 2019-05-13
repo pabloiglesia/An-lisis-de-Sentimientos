03c2d566ae5f5648920869764e7ba97f4a5ce70a
pablo <pabloiglesia@outlook.com>
2019-05-13 19:24:26 +0200

    Cambios en el traductor

--- src/main/java/asr/proyectoFinal/services/Translator.java
+++ src/main/java/asr/proyectoFinal/services/Translator.java
@@ -8,21 +8,25 @@ public class Translator
 {
 	public static String translate(String texto, String source, String target)
 	{
-		IamOptions iamOptions = new IamOptions.Builder()
-				  .apiKey("y_SdizANmBhTex3ex5LKGIiCq1uNFv3J_9OaLpJLRQBR")
-				  .build();
-		
-		LanguageTranslator service = new LanguageTranslator("2018-10-21");
-		service.setEndPoint("https://gateway-lon.watsonplatform.net/language-translator/api");
-		service.setIamCredentials(iamOptions);
-
-		TranslateOptions translateOptions = new TranslateOptions.Builder()
-		  .addText(texto)
-		  .source(source)
-		  .target(target)
-		  .build();
-		
-		String translationResult = service.translate(translateOptions).execute().getTranslations().get(0).getTranslationOutput();
+		if(!source.equals(target)) {
+			IamOptions iamOptions = new IamOptions.Builder()
+					  .apiKey("y_SdizANmBhTex3ex5LKGIiCq1uNFv3J_9OaLpJLRQBR")
+					  .build();
+			
+			LanguageTranslator service = new LanguageTranslator("2018-10-21");
+			service.setEndPoint("https://gateway-lon.watsonplatform.net/language-translator/api");
+			service.setIamCredentials(iamOptions);
+	
+			TranslateOptions translateOptions = new TranslateOptions.Builder()
+			  .addText(texto)
+			  .source(source)
+			  .target(target)
+			  .build();
+			
+			String translationResult = service.translate(translateOptions).execute().getTranslations().get(0).getTranslationOutput();
+		} else {
+			String translationResult = texto;
+		}
 
 		return translationResult;
 	}
